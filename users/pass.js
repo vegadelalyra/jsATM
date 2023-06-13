238cb125139e3d4a54b8e7b10b8065bbd7964617
VegadelaLyra <lyradeperseo@gmail.com>
2023-06-13 18:52:43 -0500

    BEHOLD: CORS

--- users/pass.js
+++
@@ -1,4 +0,0 @@
-// submodule to generate 4 digits passwords
-export function genRanPass() {
-    
-}
--- users/type.js
+++
@@ -1,7 +0,0 @@
-// submodule to classify admin or user
-import { genRamNum } from "./randomNum"
-
-const types = {
-    [1]: 2, 
-    user : 1
-}
